d37e71ac1ca430c9ef561e687d203cae9f89109c
cristiansk8 <cdpsk8@gmail.com>
2020-07-01 13:50:04 -0500

    funtes

--- wp-content/themes/dep_robotica/includes/footer.php
+++ wp-content/themes/dep_robotica/includes/footer.php
@@ -14,3 +14,15 @@
 <?php wp_footer(); ?>
 </body>
 </html>
+
+
+<div class="container-fuid" style="background-image: url('http://nabai.co/wp-content/uploads/2020/06/formulario-foto-fondo-1.png');">
+  <div class="row">
+    <div class="col-md-6">
+      <img src="https://nabai.co/wp-content/uploads/2020/06/logo.png" alt="" width="100%" height="auto">
+    </div>
+    <div class="col-md-6">
+       <?php echo do_shortcode('[contact-form-7 id="10" title="Contact form 1"]'); ?>
+    </div>
+  </div>
+</div>
